78320d99259b3cb7d9b88278aa232daa91b0df24
kiridharan <kiridharan217dec2001@gmail.com>
2022-04-16 20:08:37 +0530

    secret key resolved

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9-(jg2m)tdr90_g4%*6c&zh85z)ic%dcly*eh0b7sffa&59i63'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
